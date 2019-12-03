e6c73ecfe3903d84fdb434f408e61131c5981baa
Jason McGowan <jasonmcgowan14@gmail.com>
2019-12-03 08:48:43 -0500

    Updates

---
+++ precache-manifest.8bd187de06b1a770d112912990b17bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a482849f74634e4356f4123f589ccb5",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "fdee6d53c88cf304452b",
+    "url": "/clickygame/static/css/main.a969f405.chunk.css"
+  },
+  {
+    "revision": "9fafe9914a3af2b825c0",
+    "url": "/clickygame/static/js/2.385dfd2c.chunk.js"
+  },
+  {
+    "revision": "fdee6d53c88cf304452b",
+    "url": "/clickygame/static/js/main.f4f7e2b6.chunk.js"
+  },
+  {
+    "revision": "52c0ac3119a7ccf8c008",
+    "url": "/clickygame/static/js/runtime-main.84c6f054.js"
+  }
+]);
